1225f8fc4a5aef536cf8ed723686548d00848d21
Patrick Sheridan <sheridp2@gmail.com>
2017-09-06 14:36:49 -0700

    landing component scaffolding added and imported into app component

---
+++ src/component/landing/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Triviapp Landing Page</h1>
+        <p>Description will go here</p>
+        <button>Start Game</button>
+      </div>
+    );
+  }
+}
+
+export default Landing;
