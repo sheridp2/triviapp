d49ae98f09c503470cc81875dd7332e2df5f6885
Shelly Tang <shellytang@gmail.com>
2017-09-07 15:53:21 -0700

    received data from api and working on rendering to the view

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Landing from '../landing';
 import Dashboard from '../dashboard';
+import axios from 'axios';
 import { BrowserRouter, Route} from 'react-router-dom';
 
 class App extends React.Component {
+
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/play" component={Dashboard} />
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/play" component={Dashboard} question={this.state}/>
         </div>
       </BrowserRouter>
     );
--- src/component/dashboard/index.js
+++ src/component/dashboard/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Counter from '../counter'
+import axios from 'axios'
+import QuestionContainer from '../question-container'
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {questions: []};
+  }
+
+  componentDidMount() {
+    axios.get('https://opentdb.com/api.php?amount=5&difficulty=easy')
+      .then(res => {
+        // console.log('data', res);
+        this.setState({questions: res}, () => {
+          console.log('STATE: ', this.state.questions);
+        });
+      });
+
+  }
+
   render() {
     return (
       <div>
         <h1>Hello this is the game page</h1>
+        <QuestionContainer question={this.state.questions} />
         <Counter />
         <Counter />
         <Counter />
