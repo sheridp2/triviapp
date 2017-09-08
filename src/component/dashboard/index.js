82f9c6b49aa8498087d1f4ad01f9482c4b36093a
Patrick Sheridan <sheridp2@gmail.com>
2017-09-08 13:25:43 -0700

    debug of special character issue in questions

--- src/component/dashboard/index.js
+++ src/component/dashboard/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import Counter from '../counter'
-import axios from 'axios'
-import QuestionContainer from '../question-container'
+import React from 'react';
+import Counter from '../counter';
+import axios from 'axios';
+import QuestionContainer from '../question-container';
+
+function decodeEntities(input) {
+  var y = document.createElement('textarea');
+  y.innerHTML = input;
+  return y.value;
+}
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -13,33 +19,40 @@ class Dashboard extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://opentdb.com/api.php?amount=5&difficulty=easy')
+    axios
+      .get('https://opentdb.com/api.php?amount=5&difficulty=easy')
       .then(questions => {
         // console.log('WHAT', questions)
         let res = questions.data.results;
         let triviaQuestion = res.map(item => {
-          return item.question;
+          let decoded = decodeEntities(item.question);
+          return decoded;
         });
         let triviaAnswer = res.map(item => {
           return item.correct_answer;
         });
 
-        this.setState({
-          currentQuestion: triviaQuestion[0],
-          currentAnswer: triviaAnswer[0],
-        }, () => {
-          console.log('STATE: ', this.state);
-        });
+        this.setState(
+          {
+            currentQuestion: triviaQuestion[0],
+            currentAnswer: triviaAnswer[0],
+          },
+          () => {
+            console.log('STATE: ', this.state);
+          }
+        );
       })
       .catch(console.error);
-
   }
 
   render() {
     return (
       <div>
         <h1>Hello this is the game page</h1>
-        <QuestionContainer    currentQuestion={this.state.currentQuestion} currentAnswer={this.state.currentAnswer} />
+        <QuestionContainer
+          currentQuestion={this.state.currentQuestion}
+          currentAnswer={this.state.currentAnswer}
+        />
         <Counter />
         <Counter />
         <Counter />
