8618d860a846ee46ead4584e718c4d4c742be435
Carlo Santos <carlox7@gmail.com>
2017-09-14 16:59:08 -0700

    difficulty selector started, needs to change difficulty state

--- src/component/difficulty-container/index.js
+++ src/component/difficulty-container/index.js
@@ -1,6 +1,48 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class Difficulty extends Component {
-  
+class DifficultyContainer extends Component {
+  constructor(props){
+    super(props);
+
+    console.log('this is the difficulty', this.props.difficulty);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillMount(){
+
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.setState({difficulty: e.target.value});
+    console.log(this.state);
+  }
+
+  handleChange(e){
+    e.preventDefault();
+    console.log('button was clicked', e.target.value);
+    this.setState({difficulty: e.target.value});
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <select value={this.props.value} onChange={this.handleChange}>
+            <option selected disabled hidden>Choose Difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+          <button type="submit" name="submit">Select</button>
+        </form>
+      </div>
+
+    );
+  }
 }
+
+export default DifficultyContainer;
