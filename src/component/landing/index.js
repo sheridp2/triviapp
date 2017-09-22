997cc1753afe2f45f46ac937ff7d3d7294e59096
Carlo Santos <carlox7@gmail.com>
2017-09-22 14:36:09 -0700

    fixed spacing for player div, changed colors, added red underline to h1

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -5,7 +5,7 @@ class Landing extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome to Triviapp Landing Page</h1>
+        <h1>Welcome to Guess Work!</h1>
         <p>Description will go here</p>
         <Link to='/play'>Start Game</Link>
       </div>
